283b8b20fc52ca12ab1fe28ff866abfc6749fcca
lpm0073 <lpm0073@gmail.com>
2023-11-30 15:10:25 -0600

    chore: add functional unit tests

--- models/tests/test_base.py
+++ models/tests/test_base.py
@@ -6,13 +6,35 @@ Test integrity of base class.
 """
 import pytest  # pylint: disable=unused-import
 
+from ..prompt_templates import NetecPromptTemplates
 from ..ssm import SalesSupportModel
 
 
 class TestSalesSupportModel:
     """Test SalesSupportModel class."""
 
+    ssm = SalesSupportModel()
+    templates = NetecPromptTemplates()
+
     def test_01_basic(self):
         """Test a basic request"""
 
         SalesSupportModel()
+
+    def test_oracle_training_services(self):
+        """Test a prompt with the Oracle training services template"""
+
+        prompt = self.templates.oracle_training_services
+        result = self.ssm.prompt_with_template(prompt=prompt, concept="Oracle database administrator")
+        assert result
+        assert "Oracle" in result
+        assert "training" in result
+
+    def test_training_services(self):
+        """Test a prompt with the training services template"""
+
+        prompt = self.templates.training_services
+        result = self.ssm.prompt_with_template(prompt=prompt, concept="Microsoft certified Azure AI engineer associate")
+        assert result
+        assert "Microsoft" in result
+        assert "training" in result
