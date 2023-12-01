bad699481d217dde81877d85124395529652dabe
lpm0073 <lpm0073@gmail.com>
2023-12-01 13:19:54 -0600

    fix: had to switch to bm25_encoder so that vector store is searchable

--- models/tests/test_ssm.py
+++ models/tests/test_ssm.py
@@ -6,10 +6,9 @@ Test integrity of base class.
 import pytest  # pylint: disable=unused-import
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores.pinecone import Pinecone
+from pinecone import Index
 
-from ..ssm import SalesSupportModel
+from models.ssm import SalesSupportModel, TextSplitter
 
 
 class TestSalesSupportModel:
@@ -18,13 +17,17 @@ class TestSalesSupportModel:
     def test_01_basic(self):
         """Ensure that we can instantiate the class."""
 
-        SalesSupportModel()
+        # pylint: disable=broad-except
+        try:
+            SalesSupportModel()
+        except Exception as e:
+            assert False, f"initialization of SalesSupportModel() failed with exception: {e}"
 
     def test_02_class_aatribute_types(self):
         """ensure that class attributes are of the correct type"""
 
         ssm = SalesSupportModel()
         assert isinstance(ssm.chat, ChatOpenAI)
-        assert isinstance(ssm.pinecone_index, Pinecone)
-        assert isinstance(ssm.text_splitter, RecursiveCharacterTextSplitter)
-        assert isinstance(ssm.openai_embedding, OpenAIEmbeddings)
+        assert isinstance(ssm.pinecone_index, Index)
+        assert isinstance(ssm.text_splitter, TextSplitter)
+        assert isinstance(ssm.openai_embeddings, OpenAIEmbeddings)
